75c9405859d664cb67fc7c47d704e3a6824349ac
saerx <saerlaith.robyn@gmail.com>
2020-11-04 19:24:47 +0000

    homework complete

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -22,10 +22,24 @@ def join_string(string_1, string_2):
 def add_string_as_number(string_1, string_2):
     return int(string_1) + int(string_2)
 
-
+#It's not clear to me why these functions are repeated in the test file
+#are we supposed to do them mutliple times?
 import calendar
 def number_to_full_month_name(month_num):
     return calendar.month_name[month_num]
 
 def number_to_short_month_name(month_num):
     return calendar.month_abbr[month_num]
+
+def volume_of_cube(num_1):
+    return pow(num_1, 3)
+    #return num_1**3 [tried this too but the above reads nicer to me]
+
+def reverse_string(string_1):
+    return "".join(reversed(string_1))
+    #also read that return string_1[::-1] would be possible and if faster
+    # but it seems less readable and I understand it less
+
+def fahrenheit_to_celsius(fahr):
+    return (fahr - 32) * 5/9
+    #They are same at -40
--- tests/python_functions_test.py
+++ tests/python_functions_test.py
@@ -51,17 +51,14 @@ class TestPythonFunctionPractice(unittest.TestCase):
       result = number_to_full_month_name( 9 )
       self.assertEqual( "September", result )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_1(self):
       first_month_string = number_to_short_month_name( 1 )
       self.assertEqual( "Jan", first_month_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_4(self):
       fourth_month_string = number_to_short_month_name( 4 )
       self.assertEqual( "Apr", fourth_month_string )
 
-  @unittest.skip("delete this line to run the test")
   def test_number_to_short_month_name__month_10(self):
       tenth_month_string = number_to_short_month_name( 10 )
       self.assertEqual( "Oct", tenth_month_string )
@@ -69,22 +66,19 @@ class TestPythonFunctionPractice(unittest.TestCase):
   #Further
 
   #Given the length of a side of a cube calculate the volume
-  @unittest.skip("delete this line to run the test")
   def test_volume_of_cube(self):
-    #add test code here
-    pass
+    result = volume_of_cube( 3 )
+    self.assertEqual( 27, result )
 
   #Given a String, return the String reversed
-  @unittest.skip("delete this line to run the test")
   def test_reverse_string(self):
-    #add test code here
-    pass
+    result = reverse_string( "Palindrome" )
+    self.assertEqual( "emordnilaP", result )
 
   #Given a value in farenheit, convert this into celsius.
-  @unittest.skip("delete this line to run the test")
   def test_fahrenheit_to_celsius(self):
-    #add test code here
-    pass
+    result = fahrenheit_to_celsius( -40 )
+    self.assertEqual( -40, result )
 
 
 if __name__ == '__main__':
