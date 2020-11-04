5588964b2468e6b9618b631d1a285f1cb68d4bb6
saerx <saerlaith.robyn@gmail.com>
2020-11-04 18:56:10 +0000

    first tests complete

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,2 +1,31 @@
 def return_10():
     return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string_1):
+    return len(string_1)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+
+import calendar
+def number_to_full_month_name(month_num):
+    return calendar.month_name[month_num]
+
+def number_to_short_month_name(month_num):
+    return calendar.month_abbr[month_num]
